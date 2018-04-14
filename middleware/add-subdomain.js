50e2f19c07b0cf82e79d7a8deda4892e9f860c3f
Josh Bradley <joshua.bradley@procore.com>
2018-04-14 01:26:11 -0700

    Changed spacing

--- middleware/add-subdomain.js
+++ middleware/add-subdomain.js
@@ -1,9 +1,9 @@
 const Subdomain = require('../models/subdomain.js');
 
 async function saveSubdomain(options, callback) {
-	new Subdomain(options.subdomain).save().then(callback()).catch((err)=>{
-	  console.log(err.message);
-	});
+  new Subdomain(options.subdomain).save().then(callback()).catch((err)=>{
+    console.log(err.message);
+  });
 }
 
 module.exports = saveSubdomain;

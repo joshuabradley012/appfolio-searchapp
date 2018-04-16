1b0b3119e8e6fd155084de40436e6f1a0f72287b
Josh Bradley <joshua.bradley@procore.com>
2018-04-16 12:53:09 -0700

    Updated template to catch errors

---
+++ middleware/add-subdomain.js
@@ -0,0 +1,9 @@
+const Subdomain = require('../models/subdomain.js');
+
+async function saveSubdomain(options, callback) {
+  new Subdomain(options.subdomain).save().then(callback()).catch((err)=>{
+    console.log(err.message);
+  });
+}
+
+module.exports = saveSubdomain;
